a962ca98874f9f8ab3b39988e5eeabe8f0ad19f1
Augusto Samame <augustosamame@gmail.com>
2026-05-05 22:27:57 -0500

    schema changes

--- apps/backend/db/schema.rb
+++ apps/backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_29_000001) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_01_000001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -165,6 +165,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_29_000001) do
     t.string "encrypted_password", default: "", null: false
     t.string "first_name"
     t.float "formality_level"
+    t.boolean "is_tester", default: false, null: false
     t.float "last_latitude"
     t.float "last_longitude"
     t.string "last_name"
